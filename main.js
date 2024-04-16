69eb3451cf8e9e190ffe7bbf95afa3c77c75d5bf
BALJITSINGH23 <baljitrandhawa02@gmail.com>
2024-04-16 13:46:58 -0700

    Add slider in hero section

--- main.js
+++ main.js
@@ -3,3 +3,22 @@ function showNav() {
     var element = document.getElementById("nav-items");
     element.classList.toggle("show-items");
   }
+
+
+
+  
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  let currentSlide = 0;
+  const slides = document.querySelectorAll(".slide");
+  const slideCount = slides.length;
+
+  function nextSlide() {
+    slides[currentSlide].classList.remove("active");
+    currentSlide = (currentSlide + 1) % slideCount;
+    slides[currentSlide].classList.add("active");
+  }
+
+  setInterval(nextSlide, 5000); // Change slide every 5 seconds
+});
